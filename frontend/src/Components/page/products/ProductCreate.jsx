c6838fd5af69338de8858df6f1da10cb9507c8d1
TristanDereigne <tristandereigne@gmail.com>
2025-02-05 12:26:08 +0100

    feature: improve navigation + handle cta change over page

---
+++ frontend/src/Components/page/products/ProductCreate.jsx
@@ -0,0 +1,10 @@
+function ProductCreate() {
+
+    return (
+
+        <h1 className="text-xl text-black">Hello produit create</h1>
+
+    )
+}
+
+export default ProductCreate
---
+++ frontend/src/Components/page/products/ProductModify.jsx
@@ -0,0 +1,10 @@
+function ProductModify() {
+
+    return (
+
+        <h1 className="text-xl text-black">Hello produit modify</h1>
+
+    )
+}
+
+export default ProductModify
