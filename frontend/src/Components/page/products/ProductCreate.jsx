bfe93370a0424ef85fd0c9e1af2322b17fad0bbf
TristanDereigne <tristandereigne@gmail.com>
2025-02-05 17:01:49 +0100

    feature: improve creating page+routing

--- frontend/src/Components/page/products/ProductCreate.jsx
+++ frontend/src/Components/page/products/ProductCreate.jsx
@@ -1,48 +1,48 @@
 function ProductCreate() {
   
   return (
-    <div className="p-6  bg-white rounded-lg shadow mt-0">
-      <h1 className="text-xl text-black mb-4">Créer un produit</h1>
+    <div className="p-6 bg-white rounded-lg ">
+      <h1 className="text-xl text-black font-bold mb-4">Créer un produit</h1>
       <div className="space-y-4">
         <div>
-          <label className="block text-gray-700">Nom du produit</label>
-          <input className="bg-gray-100 p-2 w-full rounded" placeholder="Entrez le nom du produit" />
+          <label className="block text-black font-bold">Nom du produit</label>
+          <input className="bg-[#EBEBEB] p-2 w-full rounded" placeholder="RTX 5090 TI Strix Asus" />
         </div>
         <div>
-          <label className="block text-gray-700">Description</label>
-          <textarea className="w-full p-2 bg-gray-100 rounded-md resize-none" placeholder="Entrez la description du produit" />
+          <label className="block text-black font-bold">Description</label>
+          <textarea className="w-full p-2 bg-[#EBEBEB] rounded-md resize-none truncate" placeholder="La plus puissante des cartes graphiques, le tout nouveau GPU vous offrira ..." />
         </div>
         <div className="grid grid-cols-4 gap-4">
           <div>
-            <label className="block text-gray-700">Prix d'achat (€)</label>
-            <input type="number" className="bg-gray-100 p-2 w-full rounded" placeholder="0" />
+            <label className="block text-black font-bold">Prix d'achat (€)</label>
+            <input type="number" className="bg-[#EBEBEB] p-2 w-full rounded" placeholder="0" />
           </div>
           <div>
-            <label className="block text-gray-700">Statut</label>
-            <select className="bg-gray-100 p-2 w-full rounded">
+            <label className="block text-black font-bold">Statut</label>
+            <select className="bg-[#EBEBEB] p-2 w-full rounded">
               <option value="">Choisir</option>
               <option value="Disponible">Disponible</option>
               <option value="En rupture">En rupture</option>
             </select>
           </div>
           <div>
-            <label className="block text-gray-700">Fournisseur</label>
-            <select className="bg-gray-100 p-2 w-full rounded">
+            <label className="block text-black font-bold">Fournisseur</label>
+            <select className="bg-[#EBEBEB] p-2 w-full rounded">
               <option value="">Choisir</option>
               <option value="Asus">Asus</option>
               <option value="MSI">MSI</option>
             </select>
           </div>
           <div>
-            <label className="block text-gray-700">Catégorie</label>
-            <select className="bg-gray-100 p-2 w-full rounded">
+            <label className="block text-black font-bold">Catégorie</label>
+            <select className="bg-[#EBEBEB] p-2 w-full rounded">
               <option value="">Choisir</option>
               <option value="Carte graphique">Carte graphique</option>
               <option value="Périphérique">Périphérique</option>
             </select>
           </div>
         </div>
-        <button className="w-full mt-4 text-lg bg-blue-600 text-white p-3 rounded">Créer</button>
+        <button className="w-full mt-4 text-lg bg-[#4857FF] text-white p-3 rounded-[16px] cursor-pointer">Créer</button>
       </div>
     </div>
   );
